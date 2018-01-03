652e5a21b914368cb3bfb6cc6876f214902cbeea
Omar <34662089+OmarMesqq@users.noreply.github.com>
2018-01-02 22:38:33 -0200

    Fixed millions of bugs
    There's one left though. I use Visual Studio Community 2017 and I keep getting the error C2248. If someone manages to fix it I would be extremely honored

--- BankHelper.h
+++ BankHelper.h
@@ -7,82 +7,147 @@
 #include <string>
 
 class BankAccount {   //aspects of the bank account
-BankAccount() : Balance(0.0) {}
-~BankAccount() {
-  std::cout << "It was an honor to have you in CppBank. Goodbye!\n\n" << std::endl;
-}
+	BankAccount() {
+		Balance = 0.0;
+	}
+	~BankAccount() {
+		std::cout << "It was an honor to have you in CppBank. Goodbye!\n\n" << std::endl;
+	}
 public:
-  void Deposit();
-  void Withdraw();
-  void setName();
-  void CloseAccount()
-  BankAccount * ptr = this;
+	void Deposit(double AmmIn) {
+		std::cout << "How much are you willing to deposit?" << std::endl;
+		std::cin >> AmmIn;
+		std::cout << "Operation successful!" << std::endl;
+		Balance += AmmIn;
+
+	}
+	void Withdraw(double AmmOut) {
+		std::cout << "How much are you willing to withdraw?" << std::endl;
+		std::cin >> AmmOut;
+
+		if (AmmOut > Balance) {
+			std::cout << "You do not have this much money" << std::endl;
+		}
+		else {
+			std::cout << "Operation successful!" << std::endl;
+			Balance -= AmmOut;
+		}
+	}
+	void setName(std::string x) {
+		Name = x;
+	}
+	void CloseAccount() {
+		delete ptr; //deletes pointer to created object, destroying it 
+	}
+	void setPassword(std::string x) {
+		passwd = x;
+	}
+	BankAccount * ptr = this;
 private:
-  const unsigned short AccNum;
-  string passwd;
-  const string Name;
-  long double Balance;
-  friend void setPassword();
+	unsigned short AccNum;
+	std::string passwd;
+	std::string Name;
+	long double Balance;
 };
 
+unsigned short AccGen() {
+	srand(time(NULL));  //entropy seed : local time
+	return 1 + (rand() % 99);    //returns value between 1 and 99 (range of the bank accounts IDs)
+}
+
 void CreateAccount() {
-  BankAccount NewAccount; //creates the object "NewAccount" from class "BankAccount"
-  ptr = &NewAccount; //assigns the pointer to the created object in case of account deletion
+	BankAccount NewAccount; //creates the object "NewAccount" from class "BankAccount"
+	BankAccount * ptr = &NewAccount; //assigns the pointer to the created object in case of account deletion
 
-  std::cout << "Enter your name:\n" << std::endl;   //sets name
-  string x;
-  std::cin >> x;
-  BankAccount.setName(x); 
+	std::cout << "Enter your name:\n" << std::endl;   //sets name
+	std::string x;
+	std::cin >> x;
+	NewAccount.setName(x);
 
-  std::cout << "Set a password:\n" << std::endl;  //sets password
-  string pwd;
-  ptr->setPassword(pwd); //same as using BankAccount.setPassword (pwd)
+	std::cout << "Set a password:\n" << std::endl;  //sets password
+	std::string pwd;
+	ptr->setPassword(pwd); //same as using BankAccount.setPassword (pwd)
 
-  std::cout << "Please write down your account number:\n" << std::endl;  //sets account number
-  std::cout << AccGen() << std::cout;
-
-}
- 
-
-short AccGen() {
-  srand(time(NULL));  //entropy seed : local time
-  return 1 + (rand() % 99);    //returns value between 1 and 99 (range of the bank accounts IDs)
-}
-
-void Deposit(double AmmIn) {
-  std::cout << "How much are you willing to deposit?" << std::endl;
-  std::cin >> AmmIn;
-  std::cout << "Operation successful!" << std::endl;
-  Balance += AmmIn
+	std::cout << "Please write down your account number:\n" << std::endl;  //sets account number
+	std::cout << AccGen();
 
 }
 
-void Withdraw(double AmmOut) {
-  std::cout << "How much are you willing to withdraw?" << std::endl;
-  std::cin >> AmmOut;
+#endif // BANK_HELPER_H
 
-  if (AmmOut > Balance ) {
-    std::cout << "You do not have this much money" << std::endl;
-  }
-  else {
-    std::cout << "Operation successful!" << std::endl;
-    Balance -= AmmOut;
-  }
+
+#ifndef BANK_HELPER_H 
+#define BANK_HELPER_H
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+
+class BankAccount {   //aspects of the bank account
+	BankAccount() : Balance(0.0) {}
+	~BankAccount() {
+		std::cout << "It was an honor to have you in CppBank. Goodbye!\n\n" << std::endl;
+	}
+public:
+	void Deposit(double AmmIn) {
+		std::cout << "How much are you willing to deposit?" << std::endl;
+		std::cin >> AmmIn;
+		std::cout << "Operation successful!" << std::endl;
+		Balance += AmmIn;
+
+	}
+	void Withdraw(double AmmOut) {
+		std::cout << "How much are you willing to withdraw?" << std::endl;
+		std::cin >> AmmOut;
+
+		if (AmmOut > Balance) {
+			std::cout << "You do not have this much money" << std::endl;
+		}
+		else {
+			std::cout << "Operation successful!" << std::endl;
+			Balance -= AmmOut;
+		}
+	}
+	void setName(std::string x) {
+		Name = x;
+	}
+	void CloseAccount() {
+		delete ptr; //deletes pointer to created object, destroying it 
+	}
+	void setPassword(std::string x) {
+		passwd = x;
+	}
+	BankAccount * ptr = this;
+private:
+	unsigned short AccNum;
+	std::string passwd;
+	std::string Name;
+	long double Balance;
+};
+
+unsigned short AccGen() {
+	srand(time(NULL));  //entropy seed : local time
+	return 1 + (rand() % 99);    //returns value between 1 and 99 (range of the bank accounts IDs)
 }
 
-void setName(string x) {
-  Name = x;
+void CreateAccount() {
+	BankAccount NewAccount; //creates the object "NewAccount" from class "BankAccount"
+	BankAccount * ptr = &NewAccount; //assigns the pointer to the created object in case of account deletion
+
+	std::cout << "Enter your name:\n" << std::endl;   //sets name
+	std::string x;
+	std::cin >> x;
+	ptr->setName(x);
+
+	std::cout << "Set a password:\n" << std::endl;  //sets password
+	std::string pwd;
+	ptr->setPassword(pwd);
+
+	std::cout << "Please write down your account number:\n" << std::endl;  //sets account number
+	std::cout << AccGen();
+
 }
-
-void setPassword(string x) {
-  passwd = x;
-}
-
-void CloseAccount() {
-  delete ptr;  //deletes pointer to created object, destroying it
-}
-
-
 
 #endif // BANK_HELPER_H
 
