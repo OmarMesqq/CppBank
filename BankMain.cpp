8f65f474d59581492fa74730dde32c549cdee9da
Omar <34662089+OmarMesqq@users.noreply.github.com>
2018-01-07 01:47:49 -0200

    Changed few things
    in order to keep up with the header file

--- BankMain.cpp
+++ BankMain.cpp
@@ -1,15 +1,16 @@
-// CppBank v 0.1
+// CppBank v 0.2
 #include <iostream>
 #include "BankHelper.h"
 
-int main(){
-  std::cout << "Welcome to CppBank!" << std::endl;
-  std::cout << "Let's create your account" << std::endl;
-  CreateAccount();
-
+int main() {
+	std::cout << "Welcome to CppBank!" << std::endl;
+	std::cout << "Let's create your account" << std::endl;
+	CreateAccount();
+	return 0;
 }
 
 
 //VERSION TRACKING:
 
-// v0.1: initial release
+// v0.1: initial commit
+// v0.2: functioning release
