48754713e8447b1de0f32b2d92c646cac1bbd8ff
Omar <34662089+OmarMesqq@users.noreply.github.com>
2020-11-29 00:23:11 -0300

    Update BankMain.cpp

--- BankMain.cpp
+++ BankMain.cpp
@@ -3,9 +3,20 @@
 #include "BankHelper.h"
 
 int main() {
-	std::cout << "Welcome to CppBank!" << std::endl;
+	
+	std::cout << R"(  _____  _           ____              _    
+ |  __ \| |         |  _ \            | |   
+ | |__) | |_   _ ___| |_) | __ _ _ __ | | __
+ |  ___/| | | | / __|  _ < / _` | '_ \| |/ /
+ | |    | | |_| \__ \ |_) | (_| | | | |   < 
+ |_|    |_|\__,_|___/____/ \__,_|_| |_|_|\_\
+                                            
+                                           )"  << std::endl;
+	
 	std::cout << "Let's create your account" << std::endl;
+	
 	CreateAccount();
+	
 	return 0;
 }
 
