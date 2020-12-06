312dc0bb5cba81f311b14ef8462c901ec806dd3b
Omar <34662089+OmarMesqq@users.noreply.github.com>
2020-12-06 14:18:55 -0300

    Update BankMain.cpp

--- BankMain.cpp
+++ BankMain.cpp
@@ -1,15 +1,40 @@
-// CppBank v 0.1
+// PlusBank v 0.4
+
+
 #include <iostream>
 #include "BankHelper.h"
 
-int main(){
-  std::cout << "Welcome to CppBank!" << std::endl;
-  std::cout << "Let's create your account" << std::endl;
-  CreateAccount();
+using namespace std;
 
+int main() {
+
+	cout << R"(  _____  _           ____              _    
+ |  __ \| |         |  _ \            | |   
+ | |__) | |_   _ ___| |_) | __ _ _ __ | | __
+ |  ___/| | | | / __|  _ < / _` | '_ \| |/ /
+ | |    | | |_| \__ \ |_) | (_| | | | |   < 
+ |_|    |_|\__,_|___/____/ \__,_|_| |_|_|\_\
+                                            
+                                           )" << endl;
+
+
+
+	cout << "1 - Create account\n";
+	cout << "2 - Login\n";
+
+	int choice;
+	cin >> choice;
+
+	if (choice == 1) {
+		cout << "\n";
+		CreateAccount();
+		main();
+
+	}
+
+
+	return 0;
 }
 
 
-//VERSION TRACKING:
-
-// v0.1: initial release
+//TO-DO:
