f3e5172d699a68d9b96f58495028a1a09b91ae1b
Omar <34662089+OmarMesqq@users.noreply.github.com>
2020-12-06 14:18:32 -0300

    Made code simpler
    And removed unnecessary pointers

--- BankHelper.h
+++ BankHelper.h
@@ -1,76 +1,107 @@
+#ifndef BANK_HELPER_H 
+#define BANK_HELPER_H
+
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <string>
+#include <fstream>
 
-#define TRUE = 0   //defining booleans
-#define FALSE = 1
+unsigned short AccGen() {
+	srand(time(NULL));  //entropy seed : local time
+
+	return 1 + (rand() % 99);    //returns value between 1 and 99 (range of the bank accounts IDs)
+}
 
 class BankAccount {   //aspects of the bank account
+
 public:
-  long double Balance;
-  void Deposit();
-  void Withdraw();
-  char Name[256];
-  bool IsOnline;
-  BankAccount(){
-    Balance = 0.0;
-  };
-  ~BankAccount();
+
+	BankAccount() {
+		Balance = 0.0;
+	}
+
+	void Deposit(double AmmIn) {
+		std::cout << "How much are you willing to deposit?" << std::endl;
+		std::cin >> AmmIn;
+		std::cout << "Operation successful!" << std::endl;
+		Balance += AmmIn;
+
+	}
+	void Withdraw(double AmmOut) {
+		std::cout << "How much are you willing to withdraw?" << std::endl;
+		std::cin >> AmmOut;
+
+		if (AmmOut > Balance) {
+			std::cout << "You do not have this much money" << std::endl;
+		}
+		else {
+			std::cout << "Operation successful!" << std::endl;
+			Balance -= AmmOut;
+		}
+	}
+
+	void setName(std::string x) {
+		Name = x;
+	}
+
+	void setPassword(std::string x) {
+		passwd = x;
+	}
+	void setAccNum() {
+		AccNum = AccGen();
+	}
+
+	auto getName() {
+		return Name;
+	}
+
+	auto getAccNum() {
+		return AccNum;
+	}
+
 private:
-  unsigned short AccNum;
-  char passwd[256];
+
+	unsigned short AccNum;
+	std::string passwd;
+	std::string Name;
+	long double Balance;
 };
 
-void CreateAccount() {
-  BankAccount NewAcc; //creates the object "NewAcc" from class "BankAccount"
+static void CreateAccount() {
 
-  std::cout << "Enter your full name:\n";
-  std::cin >> BankAccount::Name;
+	BankAccount NewAccount; //creates the object "NewAccount" from class "BankAccount"
+	
 
-  std::cout << "Enter your password\n";
-  std::cin >> BankAccount::passwd;
+	std::cout << "Enter your name: " << "\n";   //sets name
+	std::string x;
+	std::cin >> x;
+	NewAccount.setName(x);
 
-  std::cout << "Re-enter your password:\n";
-  char checkPasswd[256];  //check if password is correct
-  char * Bye[] = checkPasswd;  //pointer pointing to the array above for future use
-  std::cin >> checkPasswd;
+	std::cout << "Set a password: " << "\n";  //sets password
+	std::string pwd;
+	std::cin >> pwd;
 
-  if (checkPasswd==BankAccount::passwd) {
-    std::cout << "|||||||||||||||||SUMMARY||||||||||||||||\n\n";
-    std::cout << "Your name: " << BankAccount::Name << endl;
-    std::cout << "Your password: " << BankAccount::passwd << endl;
-    std::cout << "Your account number: " << AccGen() << endl;
-    delete Bye;  // deletes the pointer, thus freeing space in the stack
-  }
-  else {
-    std::cout << "Your passwords do not match" << endl;
-  }
-}
+	std::cout << "Type in your password again: " << "\n";
+	std::string chckPW;
+	std::cin >> chckPW;
 
-short AccGen() {
-  srand(time(NULL));  //high entropy seed : local time
-  return 1 + (rand() % 99);    //returns value between 1 and 99 (range of the bank accounts IDs)
-}
+	if (chckPW == pwd) {     //checks password
+		NewAccount.setPassword(pwd);
+		NewAccount.setAccNum();
 
-void Deposit(double AmmIn) {
-  std::cout << "How much are you willing to deposit?" << std::endl;
-  std::cin >> AmmIn;
-  std::cout << "Operation successful!" << std::endl;
-  Balance += AmmIn
+		std::cout << "This is your PlusBank ID Number:\n\n";
+		std::cout << "Keep it in a safe place\n\n";
+;
+		std::cout << NewAccount.getAccNum() << "\n\n";
+
+	}
+
+	else {
+		std::cout << "Your passwords do not match! Please, try again.\n\n" << std::endl;
+		CreateAccount();
+	}
 
 }
 
-void Withdraw(double AmmOut) {
-  std::cout << "How much are you willing to withdraw?" << std::endl;
-  std::cin >> AmmOut;
-
-  if (AmmOut > Balance ) {
-    std::cout << "You do not have this much money" << std::endl;
-  }
-  else {
-    std::cout << "Operation successful!" << std::endl;
-    Balance -= AmmOut;
-  }
-}
-
-
+#endif // BANK_HELPER_H
